4fcefd7c20a904eff1f588d5cf21bd1d84f66d5e
Degamisu <degamisu@gmail.com>
2024-01-22 14:05:13 -0500

    reordered autoinstall.bash
    Reordered the "Install Git" section to front, fixing an error where Git needed to be installed before running `git auth login`.

--- autoinstall.bash
+++ autoinstall.bash
@@ -1,14 +1,28 @@
-gh auth login
-wait(3)
-gh repo clone Degamisu/Console-Weather-App && cd Console-Weather-App
+# Install Git
+
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
 && sudo apt install gh -y
+
+# Clone Repo
+
+gh auth login
+wait(3)
+gh repo clone Degamisu/Console-Weather-App && cd Console-Weather-App
+
+# Install Dependencies
+
 pip install -r requirements.txt
 pyinstaller --onefile main.py
+
+# Cleanup
+
 rm -r build
+
+# Run
+
 cd dist
 ./main
